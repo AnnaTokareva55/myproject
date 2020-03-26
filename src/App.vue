47e35b50c996a7d73eef01163b8a1940028d66e4
AnnaTokareva55 <edelveis2606@yandex.ru>
2020-03-26 22:10:14 +0300

    Creating a header.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Welcome to my app!</h1>
+  <div id="app">
+    <Header />
+    <md-content>
+      <h1 class="md-title">Добро пожаловать!</h1>
+    </md-content>
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    Header
+  }
 };
 </script>
+
+<style>
+.d-flex {
+  display: flex;
+}
+
+.justify-content-between {
+  justify-content: space-between;
+}
+
+.md-content {
+  padding: 20px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <md-toolbar class="d-flex justify-content-between">
+    <div class="menu">
+      <md-button :md-ripple="false">Главная</md-button>
+      <md-button :md-ripple="false">Статья</md-button>
+    </div>
+    <div class="login">
+      <md-button class="md-primary">Войти</md-button>
+      <md-button class="md-raised md-primary">Регистрация</md-button>
+    </div>
+  </md-toolbar>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
